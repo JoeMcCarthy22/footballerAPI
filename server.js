61b59e25f0b37df0341add39b5872596e30bb78f
Joe McCarthy <joe_mccarthy22@hotmail.co.uk>
2024-11-13 17:25:23 +0000

    working footballer API

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const ballers = {
+
+    'messi': {
+    'firstName': 'lionel',
+    'Age': 34,
+    'Nation': 'argentina'
+    },
+    'ronaldo':{
+    'firstName': 'cristiano',
+    'Age': 38,
+    'Nation': 'portugal'  
+    },
+    'hazard':{
+    'firstName': 'eden',
+    'Age': 32,
+    'Nation': 'belgium'
+    },
+    'unknown':{
+    'firstName': 'unknown',
+    'Age': 'unknown',
+    'Nation': 'unknown'
+    },
+}
+
+app.get('/', (request, response) =>{
+    response.sendFile(__dirname + '/index.html')
+} )
+
+app.get('/api/:name', (request, response) =>{
+    const ballersSurname = request.params.name.toLowerCase()
+    if (ballers[ballersSurname]){
+        response.json(ballers[ballersSurname])
+    } else {
+        response.json(ballers['unknown'])
+    }
+    
+})
+
+app.listen(PORT, () => {
+    console.log(`The Server is now running on Port ${PORT}! Better go catch it!`)
+})
